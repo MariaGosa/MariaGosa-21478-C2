63006a3847af11c73433b64988a7d84826684efe
MariaGosa <mariagosa02@gmail.com>
2023-06-15 16:53:08 +0200

    Dokumentacja klasa Main
    Stworzyłam krótką dokumentacje dla klasy Main.

--- com/example/Main.java
+++ com/example/Main.java
@@ -1,13 +1,14 @@
 package com.example;
 
 import com.example.service.GameService;
+/* Główna klasa. */
 public class Main {
     public static void main(String[] args){
         GameService gameService = new GameService();
 
         gameService.startNewGame("Player1", "Player2");
         gameService.playNextRound();
-
+        /* Wyświetla wyniki na konsoli */
         System.out.println("Round: " + gameService.getCurrentRound());
         System.out.println("Player 1: " + gameService.getPlayer1().getScore());
         System.out.println("Player 2: " + gameService.getPlayer2().getScore());
