e7792d2df6054a14eaea8a8bdf71805136e88950
MariaGosa <mariagosa02@gmail.com>
2023-06-15 16:36:50 +0200

    Klasa GameServiceTest
    Uzupełniłam kod w klasie GameServiceTest.

--- com/example/service/GameServiceTest.java
+++ com/example/service/GameServiceTest.java
@@ -1,4 +1,36 @@
 package com.example.service;
 
+import com.example.model.Player;
 public class GameServiceTest {
+    private GameService gameService;
+
+    private void assertEquals(int i, int score) {
+    }
+
+
+    public void setUp() {
+        gameService = new GameService();
+        gameService.startNewGame("Player 1", "Player 2");
+    }
+
+    public void testPlayNextRound() {
+        Player player1 = gameService.getPlayer1();
+        Player player2 = gameService.getPlayer2();
+
+        assertEquals(0, player1.getScore());
+        assertEquals(0, player2.getScore());
+
+        gameService.playNextRound();
+
+        assertEquals(1, gameService.getCurrentRound());
+        assertEquals(1, player1.getScore());
+        assertEquals(0, player2.getScore());
+
+        gameService.playNextRound();
+
+        assertEquals(2, gameService.getCurrentRound());
+        assertEquals(2, player1.getScore());
+        assertEquals(0, player2.getScore());
+    }
+
 }
