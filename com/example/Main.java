1afd1df01be5b4064b072489ad5723f290f571b1
MariaGosa <mariagosa02@gmail.com>
2023-06-15 16:25:36 +0200

    Klasa Main
    Uzupełniłam klasę Main o resztę kodu, potrzebnego do uruchomienia programu.

--- com/example/Main.java
+++ com/example/Main.java
@@ -1,4 +1,15 @@
 package com.example;
 
+import com.example.service.GameService;
 public class Main {
+    public static void main(String[] args){
+        GameService gameService = new GameService();
+
+        gameService.startNewGame("Player1", "Player2");
+        gameService.playNextRound();
+
+        System.out.println("Round: " + gameService.getCurrentRound());
+        System.out.println("Player 1: " + gameService.getPlayer1().getScore());
+        System.out.println("Player 2: " + gameService.getPlayer2().getScore());
+    }
 }
