e6157040dfc7b46161f7f92610455308cd17824a
MariaGosa <mariagosa02@gmail.com>
2023-06-15 16:19:46 +0200

    Klasa GameService
    Uzupełniłam klasę GameService, klasa obsługuje teraz grę i jej logikę.

--- com/example/service/GameService.java
+++ com/example/service/GameService.java
@@ -1,4 +1,27 @@
 package com.example.service;
 
+import com.example.game.Game;
+import com.example.model.Player;
 public class GameService {
+    private Game game;
+
+    public void startNewGame(String name1, String name2) {
+        game = new Game(name1, name2);
+    }
+
+    public void playNextRound() {
+        game.playRound();
+    }
+
+    public Player getPlayer1() {
+        return game.getPlayer1();
+    }
+
+    public Player getPlayer2() {
+        return game.getPlayer2();
+    }
+
+    public int getCurrentRound() {
+        return game.getRound();
+    }
 }
