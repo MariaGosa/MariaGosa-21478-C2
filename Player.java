954f186bcaa84cc9b8fa269eae7f5d06bad2c173
MariaGosa <mariagosa02@gmail.com>
2023-06-15 15:56:07 +0200

    git commit -m Dodałam klasę Player która reprezentuję gracza w minigrze.

---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player {
+    private String name;
+    private int score;
+
+    public Player(String name){
+        this.name = name;
+        this.score = 0;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getScore(){
+        return score;
+    }
+    public void increaseScore(){
+        score++;
+    }
+}
