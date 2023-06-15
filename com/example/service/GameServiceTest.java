aa72891d1b419a2c4e1242fb9e9c52969de12135
MariaGosa <mariagosa02@gmail.com>
2023-06-15 16:55:01 +0200

    Dokumentacja Klasa GameServiceTest
    Stworzyłam krótką dokumentację dla klasy GameServiceTest. Jest to klasa testowa dla klasy GameService.

--- com/example/service/GameServiceTest.java
+++ com/example/service/GameServiceTest.java
@@ -1,18 +1,19 @@
 package com.example.service;
 
 import com.example.model.Player;
+/* Klasa testowa dla GameService. */
 public class GameServiceTest {
     private GameService gameService;
 
     private void assertEquals(int i, int score) {
     }
 
-
+    /* Metoda ustawiająca początkowe wartosci dla testów. */
     public void setUp() {
         gameService = new GameService();
         gameService.startNewGame("Player 1", "Player 2");
     }
-
+    /* Metoda testująca funkcjonalność kolejnej rundy gry. */
     public void testPlayNextRound() {
         Player player1 = gameService.getPlayer1();
         Player player2 = gameService.getPlayer2();
