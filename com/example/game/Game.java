b527232af78857a5e39d5ac4944cef5f5120c918
MariaGosa <mariagosa02@gmail.com>
2023-06-15 16:49:02 +0200

    Dokumentacja Klasa Game
    Stworzyłam dokumentację dla Klasy Game, poprawiłam istniejący wcześniej komentarz który znajdował się w wierszu 19.

--- com/example/game/Game.java
+++ com/example/game/Game.java
@@ -3,20 +3,24 @@ package com.example.game;
 import com.example.model.Player;
 
 import java.util.Random;
+
+/* Klasa reprezentująca grę. */
 public class Game {
     private Player player1;
     private Player player2;
     private int round;
 
+    /* Tworzy nową grę między dwoma graczami */
     public Game(String name1, String name2) {
         player1 = new Player(name1);
         player2 = new Player(name2);
         round = 1;
     }
+    /* Przeprowadza rundę w grze, generuje losowe wyniki dla graczy */
 
     public void playRound() {
         Random random = new Random();
-        int player1Score = random.nextInt(10) + 1; // losowanie liczby od 1 do 10
+        int player1Score = random.nextInt(10) + 1; // losuje liczbe od 1 do 10
         int player2Score = random.nextInt(10) + 1;
 
         if (player1Score > player2Score) {
@@ -27,15 +31,15 @@ public class Game {
 
         round++;
     }
-
+    /* Zwraca pierwszego gracza */
     public Player getPlayer1() {
         return player1;
     }
-
+    /* Zwraca drugiego gracza */
     public Player getPlayer2() {
         return player2;
     }
-
+    /* Zwraca numer aktualnej rundy */
     public int getRound() {
         return round;
     }
